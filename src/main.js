c4390c4990472da8fa616f392bf23fd57fc8af51
Diego Gentner <dgentner@ucol.mx>
2020-12-10 15:44:31 -0600

    factorial added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-export default class App {}
+export default class App {
+
+
+factorial(numero) {
+let x,resultado;
+resultado = 1;
+for (x = 1; x <= numero; x++) {
+    resultado = resultado * x;
+}
+return resultado;
+}
+
+
+
+
+
+
+
+} // No borrar
 
 let app = new App();
+console.log(app.factorial(5));
